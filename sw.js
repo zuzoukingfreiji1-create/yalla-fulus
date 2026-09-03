503744423fcbb43d52ee24aa53af4dbf1b296822
zuzoukingfreiji1-create <zuzoukingfreiji1@gmail.com>
2026-09-03 16:40:49 +0300

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,33 +1,20 @@
-const CACHE_NAME = 'yalla-fulus-v3';
-const ASSETS = [
+const CACHE_NAME = 'yalla-fulus-v1';
+const urlsToCache = [
+  './',
   './index.html',
   './manifest.json'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
-  self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request).then(res => {
-        if (res && res.status === 200) {
-          const clone = res.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-        }
-        return res;
-      }).catch(() => cached);
-    })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
